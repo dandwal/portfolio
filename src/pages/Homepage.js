481a4bb626b13f2d4df5d66784f2c869fdcb300f
Daniel W <danieldwalford@gmail.com>
2020-10-18 17:42:32 +0100

    Grid and react router now in place

---
+++ src/pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Homepage = () => {
+    return (<div className={"wrapper"}>
+        <div className={"col"}>
+            <h1>Homepage</h1>
+        </div>
+    </div>)
+}
